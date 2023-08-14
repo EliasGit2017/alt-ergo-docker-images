48992c36049e9abfa51cc69004fd33763cf403d0
EliasGit2017r <azouaou.bendjaballah@etu.upmc.fr>
2023-08-14 22:37:22 +0200

    start move to ocaml to generate dockerfiles

--- alt_ergo_docker_images/src/main.ml
+++ alt_ergo_docker_images/src/main.ml
@@ -1,10 +1,20 @@
-module Df = Dockerfile
+(* open Dockerfile *)
+(* open Dockerfile_cmd.Gen *)
+
+let gen_all_dockerfiles () = ()
+
+let generate_dockerfile (target : string) (all:bool) =
+  match target with
+  | "alt-ergo-2.4.1" -> ()
+  | "ae-2.4.2" -> ()
+  | _ -> if not all then
+		print_endline "Wrong target"
+	else
+		 gen_all_dockerfiles ()
 
 let () = print_endline "Hello, World!"
 
 let () =
-  print_endline "Generating Dockerfiless"
-
-
-
-  
+  let _ = Dockerfile.empty in
+  print_endline "Generating Dockerfiless";
+  generate_dockerfile "" false
